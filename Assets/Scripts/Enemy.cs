0aa128f2570589da039a971e1dddb777bf755e87
Minh Vu <minhd_vu@yahoo.com>
2021-11-25 21:29:06 -0500

    enemy documentation #4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,17 +13,22 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        health = maxHealth;
+
+        // Get the first path the enemy should follow
         GetNextPath();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Move the enemy towards the path node
         Vector3 position = new Vector3(transform.position.x, 0, transform.position.z);
         Vector3 dir = target - position;
         Debug.Log(dir);
         transform.Translate(dir.normalized * speed * Time.deltaTime);
 
+        // Give a new path node if the enemy has reach it
         if (Vector3.Distance(position, target) <= 0.01f)
         {
             GetNextPath();
@@ -32,11 +37,13 @@ public class Enemy : MonoBehaviour
 
     void GetNextPath()
     {
+        // Destroy the enemy if it has reached the tower
         if (++pathIndex >= GenerateMap.path.Count)
         {
             Destroy(gameObject);
         }
 
+        // Get the next path for the enemy to follow
         target = new Vector3(GenerateMap.path[pathIndex].x, 0, GenerateMap.path[pathIndex].y);
     }
 }
