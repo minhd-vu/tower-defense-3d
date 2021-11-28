8297c116db568842cf2ba27bce1dd965d37d1cdd
Minh Vu <minhd_vu@yahoo.com>
2021-11-28 14:49:10 -0500

    added tile highlighting #3 #2

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Color hoverColor;
+    private Color startColor;
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        // The game object has more than one material, get the surface material
+        startColor = rend.materials[1].color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        rend.materials[1].color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.materials[1].color = startColor;
+    }
+}
