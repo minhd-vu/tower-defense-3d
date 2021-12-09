6bb04cfe5ba640edaac265baca51676d6a79e96f
jeff <phijdo@gmail.com>
2021-12-09 17:59:48 -0500

    added pause functionality

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject ui;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)) {
+            Toggle();
+        }
+    }
+
+    public void Toggle() {
+        ui.SetActive( !ui.activeSelf );
+
+        if(ui.activeSelf) {
+            Time.timeScale = 0f;
+        } else {
+            Time.timeScale = 1f;
+        }
+    }
+
+}
